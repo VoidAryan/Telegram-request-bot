7531ac7b7004a15205f3cf2b7ebf41cb2c9ea560
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-03-25 17:24:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,10 @@
 
 master_id = [123456789, 987654321]
 group_id = -1001221917073
-master_username = 'Sangwan5688'
-PROJECT_NAME = "BlackHole"
-PROJECT_URL = "https://github.com/Sangwan5688/BlackHole"
-FAQ_URL = "https://github.com/Sangwan5688/BlackHole/wiki/FAQs"
-BOT_NAME = "BlackHole: Request Bot"
+master_username = '@voidxtoxic'
+PROJECT_NAME = "anime_Uploadz"
+PROJECT_URL = "https://github.com/voidxtoxic"
+BOT_NAME = "Anime Uploadz: Request Bot"
 BOT_USERNAME = "BlackHoleRequestbot"
 TOKEN = "yourToken"
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
